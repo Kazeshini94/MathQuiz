2c875fb80bf37e1f0fe5e68d8c443561b74749a7
Kazeshini94 <naux@gmx.at>
2022-09-17 05:14:14 +0200

    Finished Win and Enter Functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,14 @@ let r1; let r2; let r3
 let r4; let r5; let r6
 let r7; let r8; let r9
 
-// TODO
-// activate button per Enter on keyboard !!
-const enter = () => {
-    if (local.key === "Enter" ||
-        event.key === "Enter" ||
-        event.which === 13 )  {
-        event.default.disable()
-        $("#answerBtn").click()
-    }
-}
-
 const rng = (n) => {
     return Math.ceil(Math.random() * (n * 10))
 }
 //endregion
 
-
-
 // On Load of the Page Initialise the Questions
+// And all the Functions ! Like Pressing Button per Enter on Keyboard,
+// Reset and Check Answer Button
 $(function () {
     r1 = $("#q1").text(rng(n++) + " + " + rng(n++))
     r2 = $("#q2").text(rng(--n) + " - " + rng(n++)).hide()
@@ -35,7 +24,10 @@ $(function () {
     r9 = $("#q9").text(rng(n++) + " * " + rng(--n) + " + " + rng(n++) + " / " + rng(--n)).hide()
 
     $("img").hide()
-    $("#answerBtn").on("click", checkAnswer).on("keydown", enter)
+    window.addEventListener("keydown", function (event) {
+        enter(event)
+    })
+    $("#answerBtn").on("click", checkAnswer)
     $("#resetBtn").on("click", reset)
 })
 
@@ -44,9 +36,9 @@ const checkAnswer = () => {
     let answer = $("#answer").val()
 
     if (parseInt(answer) === eval(r1.text())) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer();
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer).on("click", checkAnswer2)
             $("#q1").hide()
@@ -54,22 +46,17 @@ const checkAnswer = () => {
             $("#answer").val("")
             $("#img1").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
 
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+    } else wrongAnswer();
 }
 
 const checkAnswer2 = () => {
     let answer = $("#answer").val()
 
     if (parseInt(answer) === eval(r2.text())) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer2).on("click", checkAnswer3)
             $("#q2").hide()
@@ -77,21 +64,17 @@ const checkAnswer2 = () => {
             $("#answer").val("")
             $("#img2").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer3 = () => {
     let answer = $("#answer").val()
 
     if (parseInt(answer) === eval(r3.text())) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer3).on("click", checkAnswer4)
             $("#q3").hide()
@@ -99,21 +82,17 @@ const checkAnswer3 = () => {
             $("#answer").val("")
             $("#img3").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer4 = () => {
     let answer = $("#answer").val()
 
     if (parseInt(answer) === parseFloat(eval(r4.text()))) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer4).on("click", checkAnswer5)
             $("#q4").hide()
@@ -121,21 +100,17 @@ const checkAnswer4 = () => {
             $("#answer").val("")
             $("#img4").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer5 = () => {
     let answer = $("#answer").val()
 
     if (parseInt(answer) === eval(r5.text())) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer5).on("click", checkAnswer6)
             $("#q5").hide()
@@ -143,21 +118,17 @@ const checkAnswer5 = () => {
             $("#answer").val("")
             $("#img5").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer6 = () => {
     let answer = $("#answer").val()
 
     if (parseInt(answer) === eval(r6.text())) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer6).on("click", checkAnswer7)
             $("#q6").hide()
@@ -165,23 +136,17 @@ const checkAnswer6 = () => {
             $("#answer").val("")
             $("#img6").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer7 = () => {
     let answer = $("#answer").val()
 
-    alert(eval(r7.text()).toFixed(2))
-
     if (parseFloat(answer) === eval(r7.text()).toFixed(2)) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer7).on("click", checkAnswer8)
             $("#q7").hide()
@@ -189,23 +154,17 @@ const checkAnswer7 = () => {
             $("#answer").val("")
             $("#img7").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer8 = () => {
     let answer = $("#answer").val()
 
-    alert(eval(r8.text()).toFixed(2))
-
     if (parseFloat(answer) === eval(r8.text()).toFixed(2)) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer8).on("click", checkAnswer9)
             $("#q8").hide()
@@ -213,44 +172,62 @@ const checkAnswer8 = () => {
             $("#answer").val("")
             $("#img8").show()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-        $("#answer").val("")
-    }, 1000)
+
+    } else wrongAnswer()
 }
 
 const checkAnswer9 = () => {
     let answer = $("#answer").val()
 
-    alert(eval(r9.text()))
-
     if (parseFloat(answer) === eval(r9.text()).toFixed(2)) {
-        $("#result").removeClass("bg-dark")
-            .addClass("bg-success")
-        setTimeout(function () {
+        rightAnswer()
+
+        setTimeout(() => {
             $("#result").removeClass("bg-success").addClass("bg-dark")
             $("#answerBtn").off("click", checkAnswer9).on("click", win)
             $("#q9").hide()
             $("#img9").show()
             $("#answer").parent().parent().remove()
         }, 1000)
-    } else $("#result").removeClass("bg-dark")
-        .addClass("bg-danger")
-    setTimeout(function () {
-        $("#result").removeClass("bg-danger").addClass("bg-dark")
-    }, 1000)
 
+    } else wrongAnswer()
+}
+
+const rightAnswer = () => {
+    $("#result").removeClass("bg-dark")
+        .addClass("bg-success")
+}
+
+const wrongAnswer = () => {
+    $("#result").removeClass("bg-dark")
+        .addClass("bg-danger")
+
+    setTimeout(() => {
+        $("#result").removeClass("bg-danger").addClass("bg-dark")
+        $("#answer").val("")
+    }, 1000)
 }
 
 const reset = () => {
     location.reload()
 }
-//endregion
 
-// TODO
-// insert new DOM-ELEMENT and remove everything else after !!
-function win() {
-
+const enter = (event) => {
+    if (event.key === "Enter" ||
+        event.code === 13 )  {
+        event.preventDefault()
+        $("#answerBtn").click()
+    }
 }
+
+function win() {
+    $("body").empty()
+        .addClass("p-5 m-0")
+        .css("background-image", "url(media/webTelescope.jpg)")
+        // .css("background-size" , "cover")
+        .append("<div class='container-fluid row g-0 p-sm-5'><p class='col-1 col-sm-3 p-lg-5'><p id='winner' class='col-6 display-1 p-xl-5 text-center text-break'>" +
+            "Congratulations on <br> Winning the Game<button id='endScreen' class='mt-5 pt-5 btn-dark text-danger bg-transparent border border-0'> Restart Game</button> </p></p></div>")
+    $("#winner").css("color", "rgb(18,215,14)")
+    $("#endScreen").on("click", reset)
+}
+//endregion
